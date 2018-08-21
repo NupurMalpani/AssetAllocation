141e031c68c875daed07a40be88d737c65324cc2
kiranmai444 <kiranmai.annem@gmail.com>
2018-08-21 17:01:22 +0530

    Goals class

--- BusinessLogic/src/com/citi/pojo/ClientResponse.java
+++ BusinessLogic/src/com/citi/pojo/ClientResponse.java
@@ -6,16 +6,29 @@ import java.util.List;
 public class ClientResponse {
 private long clientId;
 private List<Questions> questionsResponses;
+private List<ClientGoal> goals;
 public ClientResponse() {
 
 	this.clientId = -1;
 	this.questionsResponses = new ArrayList<Questions>();
+	this.goals = new ArrayList<ClientGoal>();
 }
-public ClientResponse(long clientId, List<Questions> questionsResponses) {
+
+public ClientResponse(long clientId, List<Questions> questionsResponses, List<ClientGoal> goals) {
 	super();
-	clientId = clientId;
+	this.clientId = clientId;
 	this.questionsResponses = questionsResponses;
+	this.goals = goals;
 }
+
+public List<ClientGoal> getGoals() {
+	return goals;
+}
+
+public void setGoals(List<ClientGoal> goals) {
+	this.goals = goals;
+}
+
 public long getClientId() {
 	return clientId;
 }
