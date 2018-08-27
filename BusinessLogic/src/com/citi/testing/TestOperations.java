4fee41c4e715fac62acd3fdd4db4b1728cdc9882
Krit17 <krithikaa1705@gmail.com>
2018-08-27 10:30:04 +0530

    Adding Jquery Test

---
+++ BusinessLogic/src/com/citi/testing/TestOperations.java
@@ -0,0 +1,29 @@
+package com.citi.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestOperations {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCalculateRatio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateRisk() {
+		fail("Not yet implemented");
+	}
+
+}
