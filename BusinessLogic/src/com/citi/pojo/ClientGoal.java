d398b3eeb084486540ad0b603ccf1c58baa16512
kiranmai444 <kiranmai.annem@gmail.com>
2018-08-27 16:04:21 +0530

    Goal

--- BusinessLogic/src/com/citi/pojo/ClientGoal.java
+++ BusinessLogic/src/com/citi/pojo/ClientGoal.java
@@ -5,19 +5,29 @@ private long goalId;
 private long goalAmount;
 private int currentYear;
 private int targetYear;
+private boolean goalMet;
 public ClientGoal() {
 	super();
 	this.goalId = -1;
 	this.goalAmount = -1;
 	this.currentYear = -1;
 	this.targetYear = -1;
+	this.goalMet = false;
 }
-public ClientGoal(long goalId, long goalAmount, int currentYear, int targetYear) {
+public ClientGoal(long goalId, long goalAmount, int currentYear, int targetYear,boolean goalMet) {
 	super();
 	this.goalId = goalId;
 	this.goalAmount = goalAmount;
 	this.currentYear = currentYear;
 	this.targetYear = targetYear;
+	this.goalMet = goalMet;
+}
+
+public boolean isGoalMet() {
+	return goalMet;
+}
+public void setGoalMet(boolean goalMet) {
+	this.goalMet = goalMet;
 }
 public long getGoalId() {
 	return goalId;
