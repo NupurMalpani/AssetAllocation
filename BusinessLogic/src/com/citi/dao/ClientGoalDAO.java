0d3d9fd3cd3b2f06d78f2b7b9bceb3c22db324d1
Rasika30 <rasigr8@gmail.com>
2018-08-28 12:00:05 +0530

    Updating Goals met in database.

--- BusinessLogic/src/com/citi/dao/ClientGoalDAO.java
+++ BusinessLogic/src/com/citi/dao/ClientGoalDAO.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import com.citi.connections.MyConnection;
 import com.citi.pojo.ClientGoal;
+import com.citi.pojo.Tuple;
 
 public class ClientGoalDAO {
 	public List<ClientGoal> fetchGoals(long clientId) {
@@ -38,4 +39,33 @@ public class ClientGoalDAO {
 		}
 		return goals;
 	}
+	
+	public int updateGoalsMet (Tuple<Long, List<Tuple<Long, Boolean>>> clientGoalsList) {
+		Connection conn=MyConnection.getMyConnection();
+		String UPDATE_GOAL_STATUS="UPDATE FinancialGoals SET GoalsMet=? WHERE ClientId=? AND GoalId=?";
+		PreparedStatement preparedStatement;
+		int rowsAffected=0;
+		try {
+			preparedStatement = conn.prepareStatement(UPDATE_GOAL_STATUS);
+			long clientId=clientGoalsList.getX();
+			preparedStatement.setLong(2,clientId);
+			List<Tuple<Long, Boolean>> goalsList=clientGoalsList.getY();
+			for(Tuple<Long,Boolean> goal:goalsList) {
+				preparedStatement.setBoolean(1, goal.getY());
+				preparedStatement.setLong(3, goal.getX());	
+				rowsAffected+=preparedStatement.executeUpdate();
+			}
+			if(rowsAffected==0) {
+				System.out.println("Error in updation. Cannot commit.");
+				return -1;//check this condition for not commiting
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rowsAffected;	
+	}
+	
 }
+	
