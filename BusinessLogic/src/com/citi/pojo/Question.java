16bbb54345820ed3fbc1673d1ec30dd1cc483c51
Rasika30 <rasigr8@gmail.com>
2018-08-28 17:43:05 +0530

    Changed type of response to String from char

--- BusinessLogic/src/com/citi/pojo/Question.java
+++ BusinessLogic/src/com/citi/pojo/Question.java
@@ -2,26 +2,26 @@ package com.citi.pojo;
 
 public class Question {
 private long questionId;
-private char response;
+private String response;
 private double responseValue;
 private double questionWeightage;
 
 public Question() {
-	this.response = ' ';
+	this.response = " ";
 	this.questionId = -1;
 	this.responseValue = -1;
 	this.questionWeightage = -1;
 }
 
-public char getResponse() {
+public String getResponse() {
 	return response;
 }
 
-public void setResponse(char response) {
+public void setResponse(String response) {
 	this.response = response;
 }
 
-public Question(long questionId, char response, double responseValue, double questionWeightage) {
+public Question(long questionId, String response, double responseValue, double questionWeightage) {
 	super();
 	this.questionId = questionId;
 	this.response=response;
